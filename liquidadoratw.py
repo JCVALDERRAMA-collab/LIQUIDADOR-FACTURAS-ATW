96958c41518dc62ad508f1f25fd2f227ae8db29a
JCVALDERRAMA-collab <yovalderrama30@gmail.com>
2025-07-06 20:16:10 -0500

    Update liquidadoratw.py

--- liquidadoratw.py
+++ liquidadoratw.py
@@ -144,7 +144,6 @@ if st.button("Enviar a WhatsApp Cartera",disabled=not campos_obligatorios_comple
     
     st.markdown(f'<a href="{whatsapp_url}" target="_blank" style="display: inline-block; padding: 12px 20px; background-color: #25D366; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 8px; border: none; cursor: pointer;">Abrir WhatsApp con el resumen</a>', unsafe_allow_html=True)
 
-st.markdown("---")
 if st.button("Enviar a WhatsApp Cliente",disabled=not campos_obligatorios_completos):
     whatsapp_message = f"""
 ¡Hola! Aquí está el resumen de la factura:
